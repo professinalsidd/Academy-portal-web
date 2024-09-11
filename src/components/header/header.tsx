0ef08a6e093084d2df6130fd2902b36ac62cfbb8
Siddharth <siddharth@Siddharths-MacBook-Air.local>
2024-09-11 05:49:08 +0530

    feat: dark mode icon but not working

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,20 @@
 import { Box, Typography, Button, Avatar } from "@mui/material";
-import React from "react";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
 import LogoImg from "../../assets/images/logo.png";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import { useState } from "react";
 
 type HeaderProps = {
   title: string;
 };
 
 const HeaderComp = ({ title }: HeaderProps) => {
+  const [selectedIcon, setSelectedIcons] = useState(false);
+
+  const handleSubmit = () => {
+    setSelectedIcons(!selectedIcon);
+  };
+
   return (
     <Box
       display={"flex"}
@@ -18,11 +26,24 @@ const HeaderComp = ({ title }: HeaderProps) => {
       <Box display={"flex"} justifyContent={"flex-start"}>
         <Typography pl={2}>{title}</Typography>
       </Box>
-      <Box display={"flex"} justifyContent={"flex-end"}>
+      <Box display={"flex"} justifyContent={"flex-end"} alignItems={"center"}>
         <Button variant="outlined" sx={{ mr: 2 }}>
           Join Class
         </Button>
-        <Avatar alt="Remy Sharp" sx={{ mr: 2 }} src={LogoImg} />
+        <Avatar alt="Remy Sharp" src={LogoImg} />
+        <Box
+          display={"flex"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+          onClick={handleSubmit}
+          sx={{ ml: 2, cursor: "pointer" }}
+        >
+          {selectedIcon ? (
+            <WbSunnyIcon sx={{ mr: 2 }} />
+          ) : (
+            <DarkModeIcon sx={{ mr: 2 }} />
+          )}
+        </Box>
       </Box>
     </Box>
   );
