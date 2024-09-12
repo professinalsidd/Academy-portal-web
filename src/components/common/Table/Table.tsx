3fac4af82cd8ec65ba726a190959cc5a1fdd1293
Siddharth <siddharth@Siddharths-MacBook-Air.local>
2024-09-12 05:48:53 +0530

    fix: table height issue fix it

--- src/components/common/Table/Table.tsx
+++ src/components/common/Table/Table.tsx
@@ -139,7 +139,10 @@ function TableComp() {
 
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 250 }} aria-label="custom pagination table">
+      <Table
+        sx={{ minWidth: 250, minHeight: 339 }}
+        aria-label="custom pagination table"
+      >
         <TableBody>
           {(rowsPerPage > 0
             ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
